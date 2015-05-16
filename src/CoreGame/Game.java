5ffce4bed20ecb4aa1b0d13dc8989c1adffae16a
superpirson <superpirson@gmail.com>
2015-05-16 17:25:09 -0500

    added a few file open code bits. Trying to get things ready to test the xml stuff/.

--- src/CoreGame/Game.java
+++ src/CoreGame/Game.java
@@ -2,9 +2,16 @@ package CoreGame;
 
 import static java.lang.System.out;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
+import javax.swing.JFileChooser;
 import javax.xml.bind.*;
+
+import GameData.GameData;
 
 
 public class Game {
@@ -16,7 +23,27 @@ public class Game {
 		
 		GameWindow gameWindow = new GameWindow();
 		out.println("Game window constructed. Setting up XML");
-		
+		File selectedFile = null;
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+		int result = fileChooser.showOpenDialog(null);
+		if (result == JFileChooser.APPROVE_OPTION) {
+		    selectedFile = fileChooser.getSelectedFile();
+		    
+		}
+		  InputStream mainFile;
+		  GameData mainGameData = null;
+		    try {
+		    	mainFile = new FileInputStream(selectedFile);
+		    	
+		    	mainFile.close(); 
+		    } catch (FileNotFoundException e) {
+		        // TODO Auto-generated catch block
+		        e.printStackTrace();
+		    } catch (IOException e) {
+		        // TODO Auto-generated catch block
+		        e.printStackTrace();
+		    }
 	}
 	
 	public <T> T unmarshal( Class<T> docClass, InputStream inputStream )
