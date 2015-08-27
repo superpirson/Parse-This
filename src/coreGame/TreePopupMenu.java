f7f04695c6f16fb651fc78ae545168874559999a
superpirson <sueprpirson@gmail.com>
2015-08-27 13:05:51 -0400

    Added JPopupMenu and further worked on refactorign.

---
+++ src/coreGame/TreePopupMenu.java
@@ -0,0 +1,15 @@
+package coreGame;
+
+
+
+public class TreePopupMenu extends javax.swing.JPopupMenu {
+	   JMenuItem createNewState;
+	   JMenuItem createNewChoice;
+	   JMenuItem delete;
+	public TreePopupMenu(){
+		
+		
+	}
+	
+	
+}
---
+++ src/gameObjects/GUIEditorObject.java
@@ -0,0 +1,17 @@
+package gameObjects;
+
+import java.awt.FlowLayout;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import coreGame.Game;
+
+public class GUIEditorObject {
+	public void addEditorPannel(JPanel panel) {
+		System.err.println("called unimplemented addEditorPanel methoid!");
+		return;
+	}
+}
